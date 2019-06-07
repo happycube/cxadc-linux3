95588378a84d674288ea1f37c513b194f643f111
Adam Sampson <ats@offog.org>
2019-06-08 00:38:43 +0100

    Rewrite the license comment in the kernel style.
    (From checkpatch.)

--- cxadc.c
+++ cxadc.c
@@ -1,26 +1,21 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
-    cxadc - cx2388x adc dma driver for linux 3.x
-    version 0.5
-
-    Copyright (c) 2005-2007 Hew How Chee <how_chee@yahoo.com>
-    Copyright (c) 2013-2015 Chad Page <Chad.Page@gmail.com>
-    Copyright (c) 2019 Adam Sampson <ats@offog.org>
- 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-
-*/
+ * cxadc - CX2388x ADC DMA driver for Linux, version 0.5
+ *
+ * Copyright (C) 2005-2007 Hew How Chee <how_chee@yahoo.com>
+ * Copyright (C) 2013-2015 Chad Page <Chad.Page@gmail.com>
+ * Copyright (C) 2019 Adam Sampson <ats@offog.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
 
 
 #include "cx88-reg.h"
