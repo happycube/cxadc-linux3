64fb50c195489c9e5240c86b9498b0126e8d08e0
Ethan Halsall <ethanhalsall11@augustana.edu>
2025-10-04 18:19:53 -0500

    clean up levelmon

--- levelmon.c
+++ levelmon.c
@@ -1,11 +1,12 @@
-#include <unistd.h>
-#include <stdio.h>
 #include <fcntl.h>
-#include <sys/ioctl.h>
-#include <string.h>
 #include <getopt.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <sys/ioctl.h>
 #include <sys/time.h>
+#include <unistd.h>
 
 // this needs to be one over the ring buffer size to work
 #define READ_LEN (40000000 / 4)
@@ -14,36 +15,16 @@ unsigned short *wbuf = (void *)buf;
 
 int readlen = READ_LEN;
 
-void set(char *name, char *device, int level)
-{
-	char str[512];
-
-	sprintf(str, "/sys/class/cxadc/%s/device/parameters/%s", device, name);
-
-	int fd = open(str, O_WRONLY);
-
-	sprintf(str, "%d", level);
-	write(fd, str, strlen(str) + 1);
-
-	close(fd);
-}
-
-int main(int argc, char *argv[])
-{
-
+int main(int argc, char *argv[]) {
 	FILE *syssfys;
 	int fd;
-	int level = 0;
-	int go_on = 1; // 2 after going over
 	char *device;
 	char *device_path;
 	char str[512];
 	struct timeval t1, t2;
-    double elapsedTime;
+	double elapsedTime;
 
-	int tenbit  = 0;
-	int tenxfsc = 0;
-
+	int tenbit = 0;
 	int c;
 
 	opterr = 0;
@@ -54,12 +35,6 @@ int main(int argc, char *argv[])
 
 	while ((c = getopt(argc, argv, "d:bx")) != -1) {
 		switch (c) {
-		case 'b':
-			tenbit = 1;
-			break;
-		case 'x':
-			tenxfsc = 1;
-			break;
 		case 'd':
 			if (strlen(optarg) <= 30) {
 				sprintf(device_path, "/dev/%s", optarg);
@@ -69,6 +44,7 @@ int main(int argc, char *argv[])
 		};
 	}
 
+	// open the cxadc device
 	fd = open(device_path, O_RDWR);
 	if (fd <= 0) {
 		fprintf(stderr, "%s not found\n", device_path);
@@ -80,22 +56,8 @@ int main(int argc, char *argv[])
 	}
 	close(fd);
 
-		sprintf(str, "/sys/class/cxadc/%s/device/parameters/tenbit", device);
-		syssfys = fopen(str, "r");
-
-	if (syssfys == NULL) {
-		fprintf(stderr, "no sysfs paramerters\n");
-		if (device_path)
-			free(device_path);
-		if (device)
-			free(device);
-		return -1;
-	}
-
-	fscanf(syssfys, "%d", &tenbit);
-	fclose(syssfys);
-
-	sprintf(str, "/sys/class/cxadc/%s/device/parameters/tenxfsc", device);
+	// read tenbit
+	sprintf(str, "/sys/class/cxadc/%s/device/parameters/tenbit", device);
 	syssfys = fopen(str, "r");
 
 	if (syssfys == NULL) {
@@ -106,65 +68,52 @@ int main(int argc, char *argv[])
 			free(device);
 		return -1;
 	}
-
-		fscanf(syssfys, "%d", &tenxfsc);
-		fclose(syssfys);
-
-
-	set("tenbit", device, tenbit);
-	set("tenxfsc", device, tenxfsc);
-
-	if (argc > optind) {
-		level = atoi(argv[optind]);
-
-		set("level", device, level);
-		if (device_path)
-			free(device_path);
-		if (device)
-			free(device);
-		return 0;
-	}
+	fscanf(syssfys, "%d", &tenbit);
+	fclose(syssfys);
 
 	fd = open(device_path, O_RDONLY);
-	
+
+	uint16_t min, center, max;
+	if (tenbit) {
+		min = 1;
+		max = 0x400;
+	} else {
+		min = 1;
+		max = 0x100;
+	}
+	center = max / 2;
+
 	while (1) {
-		unsigned int lo_count = 0, clip_lo = 0;
-		unsigned int hi_count = 0, clip_hi = 0;
+		uint32_t lo_count = 0, clip_lo = 0;
+		uint32_t hi_count = 0, clip_hi = 0;
 
-		unsigned int lo = 0, hi = 0, average = 0;
-		unsigned int min, center, max;
-		unsigned short low, high;
-
+		uint64_t lo = 0, hi = 0, average = 0;
+		uint16_t low, high;
 
 		// read a bit
 		gettimeofday(&t1, NULL);
 		size_t total_samples = read(fd, buf, readlen);
 		gettimeofday(&t2, NULL);
 
-    	        elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;      // sec to ms
-    	        elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;   // us to ms
+		elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;	   // sec to ms
+		elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0; // us to ms
 
 		if (tenbit) {
-			min = 1;
-			max = 0x400;
 			total_samples /= 2;
-
-			// shift unused bits
-			for (unsigned int i = 0; i < total_samples; i++) {
-				wbuf[i] = wbuf[i] >> 6;
-			}
-		} else {
-			min = 1;
-			max = 0x100;
 		}
-		
-		center = max / 2;
+
 		low = max;
 		high = min;
 
-		for (unsigned int i = 0; i < total_samples; i++) {
-			unsigned short value = (tenbit ? wbuf[i] : buf[i]) + 1;
-			
+		for (size_t i = 0; i < total_samples; i++) {
+            uint16_t value;
+            if (tenbit) {
+                value = wbuf[i] >> 6;
+            } else {
+                value = buf[i];
+            }
+            value += 1;
+
 			// find the average
 			average += value;
 
@@ -172,15 +121,15 @@ int main(int argc, char *argv[])
 			if (value < low)
 				low = value;
 			if (value > high)
-			        high = value;
-			
+				high = value;
+
 			// find the average of low and high samples
 			if (value < center) {
 				lo += value;
-			        lo_count++;
+				lo_count++;
 			} else {
 				hi += value;
-			        hi_count++;
+				hi_count++;
 			}
 
 			// flag any clipping
@@ -196,12 +145,15 @@ int main(int argc, char *argv[])
 		double avg_hi = hi / (double)max / hi_count * 100;
 		double avg_center = average / (double)total_samples / max * 100 - 50;
 		double low_pct = low / (double)max * 100;
-		double high_pct  = high / (double)max * 100;
+		double high_pct = high / (double)max * 100;
 
-		printf("lo |%d| [%7.3f%%] (%7.3f%%) center %.2f%% hi (%7.3f%%) [%7.3f%%] |%d|\tnsamp %d\trate %.2f\n", clip_lo, low_pct, avg_lo, avg_center, avg_hi, high_pct, clip_hi, total_samples, rate);
-
+		printf(
+			"lo |%d| [%7.3f%%] (%7.3f%%) center %.2f%% hi (%7.3f%%) [%7.3f%%] "
+			"|%d|\tnsamp %d\trate %.2f\n",
+			clip_lo, low_pct, avg_lo, avg_center, avg_hi, high_pct, clip_hi,
+			total_samples, rate);
 	}
-	
+
 	close(fd);
 
 	if (device_path)
@@ -210,4 +162,3 @@ int main(int argc, char *argv[])
 		free(device);
 	return 0;
 }
-
